26b6ad85ab73da6c65d6e23f8622aaad194277d1
ZYHGOD-1 <63916542+ZYHGOD-1@users.noreply.github.com>
2020-06-17 03:53:14 +0800

    加入了实时通信API

--- app/src/main/java/com/example/xueleme/AddTaskActivity.java
+++ app/src/main/java/com/example/xueleme/AddTaskActivity.java
@@ -9,6 +9,7 @@ public class AddTaskActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_add_task);
     }
 }
