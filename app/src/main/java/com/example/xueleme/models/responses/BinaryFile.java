4e2f648e4a0018dbd5668c3c5c6368cc1b1907ac
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2020-06-25 00:39:12 +0800

    实现接口IFileController，修改接口INotificatoinController的定义

---
+++ app/src/main/java/com/example/xueleme/models/responses/BinaryFile.java
@@ -0,0 +1,12 @@
+package com.example.xueleme.models.responses;
+
+import com.example.xueleme.models.ReflectiveJSONModel;
+
+import java.util.Date;
+
+public class BinaryFile extends ReflectiveJSONModel<BinaryFile> {
+    public String mD5;
+    public String fileName;
+    public String contentType;
+    public Date createdTime;
+}
